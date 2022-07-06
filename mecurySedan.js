8813bc7c5b34b763295067c98b9962560ef2ecc8
Vivecca B. Chatila <vchatila1@Viveccas-MacBook-Pro.local>
2022-07-06 19:15:08 -0400

    completed assignment

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,51 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require('./vehicle').Vehicle
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage) 
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberofWheels= 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false; 
+    }
+
+    loadPassenger(num){
+        if(this.passenger < this.maximumPassengers){
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger += num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+    start(){
+        super.start(); 
+    }
+    
+    scheduleService(mileage){
+        if(mileage > 30000){
+            this.scheduleService === true;
+        }   
+
+    }
+
+
+}
+  
+
+
+
+
+
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
